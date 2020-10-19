15388f7fbe9ad085c1cb1c20cfefa71336d12892
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-19 17:42:24 +0100

    Add selected choice display

--- resources/js/main.js
+++ resources/js/main.js
@@ -26,3 +26,38 @@ const closeRules = () => {
 }
 
 closingCross.addEventListener('click', closeRules);
+
+
+
+// Display screen with choice made by user:
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const rock = document.getElementById('rock');
+
+const sections = document.getElementsByTagName('section');
+const result = document.getElementById('result');
+
+const choiceCircle = document.getElementsByClassName('circle')[0];
+
+
+const displayPaper = (item, color) => {
+  return function() {
+    // Remove the three items from the page:
+    for (let i = 0 ; i < 3 ; i++) {
+      sections.item(i).style.display = 'none';
+    }
+    // Remove the background image of the main tag:
+    document.getElementsByTagName('main').item(0).style.backgroundImage = 'none';
+
+    // Display the selected item in first circle:
+    result.style.display = 'flex';
+    choiceCircle.classList.add('icon');
+    choiceCircle.classList.add(`${item}`);
+    choiceCircle.parentElement.classList.add('background');
+    choiceCircle.parentElement.classList.add(`${color}`);
+  }
+}
+
+paper.addEventListener('click', displayPaper('paper', 'blueColor'));
+scissors.addEventListener('click', displayPaper('scissors', 'yellowColor'));
+rock.addEventListener('click', displayPaper('rock', 'redColor'));
