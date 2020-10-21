d928e0de5fe1a5c8909ed1616b0bda5cbbfd569c
Geoffroy Vie <geoffroy.vie@gmail.com>
2020-10-21 12:40:55 +0100

    Improve comments

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,7 +9,6 @@ const displayRules = () => {
     rules.style.opacity = '1';
     rulesCounter = 1;
     rules.style.zIndex = '5';
-    
   } else {
     rules.style.opacity = '0';
     rulesCounter = 0;
@@ -17,7 +16,7 @@ const displayRules = () => {
       rules.style.zIndex = '-1';
     }, 200);
   }
-}
+};
 
 rulesButton.addEventListener('click', displayRules);
 
@@ -28,11 +27,9 @@ const closingCross = document.getElementById('close');
 const closeRules = () => {
   rulesCounter = 1;
   displayRules();
-}
+};
 
 closingCross.addEventListener('click', closeRules);
-
-
 
 // Display screen with choice made by user:
 
@@ -45,15 +42,15 @@ const result = document.getElementById('result');
 
 const choiceCircle = document.getElementsByClassName('circle')[0];
 
-
 const displayChoice = (item, color) => {
-  return function() {
+  return function () {
     // Remove the three items from the page:
-    for (let i = 0 ; i < 3 ; i++) {
+    for (let i = 0; i < 3; i++) {
       sections.item(i).style.opacity = '0';
     }
     // Remove the background image of the main tag:
-    document.getElementsByTagName('main').item(0).style.backgroundImage = 'unset';
+    document.getElementsByTagName('main').item(0).style.backgroundImage =
+      'unset';
 
     // Display the selected item in first circle:
     result.style.opacity = '1';
@@ -63,15 +60,12 @@ const displayChoice = (item, color) => {
     choiceCircle.parentElement.classList.add('background');
     choiceCircle.parentElement.classList.add(`${color}`);
     user = item;
-  }
-}
+  };
+};
 
 paper.addEventListener('click', displayChoice('paper', 'blueColor'));
 scissors.addEventListener('click', displayChoice('scissors', 'yellowColor'));
 rock.addEventListener('click', displayChoice('rock', 'redColor'));
-
-
-
 
 // Pick a random choice for the computer:
 
@@ -82,7 +76,7 @@ const displayHouseChoice = (item, color) => {
   houseCircle.classList.add(`${item}`);
   houseCircle.parentElement.classList.add('background');
   houseCircle.parentElement.classList.add(`${color}`);
-}
+};
 
 const randomChoice = () => {
   let randomNum = Math.floor(Math.random() * 3);
@@ -102,51 +96,54 @@ const randomChoice = () => {
   }
   return setTimeout(() => {
     winOrLose(user, computer);
-  }, 400); 
-}
+  }, 400);
+};
 
 paper.addEventListener('click', randomChoice);
 scissors.addEventListener('click', randomChoice);
 rock.addEventListener('click', randomChoice);
-
-
 
 // Create a reset/continue button:
 
 const resetButton = document.getElementById('reset');
 
 const resetState = () => {
-
   // Hide the result from previous choice:
   result.style.opacity = '0';
   result.style.zIndex = '-3';
-  
+
   // Remove class from previous choice:
   return setTimeout(() => {
     choiceCircle.classList.remove('paper', 'scissors', 'rock');
-    choiceCircle.parentElement.classList.remove('blueColor', 'yellowColor', 'redColor');
+    choiceCircle.parentElement.classList.remove(
+      'blueColor',
+      'yellowColor',
+      'redColor'
+    );
     houseCircle.classList.remove('paper', 'scissors', 'rock');
-    houseCircle.parentElement.classList.remove('blueColor', 'yellowColor', 'redColor');
+    houseCircle.parentElement.classList.remove(
+      'blueColor',
+      'yellowColor',
+      'redColor'
+    );
     userWinner.classList.remove('radial-grad', 'left');
     houseWinner.classList.remove('radial-grad', 'right');
     return setTimeout(() => {
-      
-      // Add the three items from the page:
-      for (let j = 0 ; j < 3 ; j++) {
-      sections.item(j).style.opacity = '1';
+      // Add the three choices to the page:
+      for (let j = 0; j < 3; j++) {
+        sections.item(j).style.opacity = '1';
       }
 
       // Display background:
-      document.getElementsByTagName('main').item(0).style.backgroundImage = 'url("resources/images/bg-triangle.svg")';
+      document.getElementsByTagName('main').item(0).style.backgroundImage =
+        'url("resources/images/bg-triangle.svg")';
     }, 500);
   }, 600);
-}
+};
 
 resetButton.addEventListener('click', resetState);
 
-
-
-// Create logic for the score:
+// Create logic for the score and sentence displayed:
 
 let user;
 let computer;
@@ -183,10 +180,10 @@ const winOrLose = (user, computer) => {
     houseWinner.classList.add('right');
   } else if (user === 'rock' && computer === 'scissors') {
     score.innerHTML = parseInt(score.innerHTML) + 1;
-    sentence.innerText = 'You win'
+    sentence.innerText = 'You win';
     userWinner.classList.add('radial-grad');
     userWinner.classList.add('left');
   } else {
-    sentence.innerText = 'Same choice, try again!'
+    sentence.innerText = 'Same choice, try again!';
   }
-}
+};
